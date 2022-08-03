c23f20e5251a77aaf7e9d13e8985fa4c2ed6ac41
wubmu <923185028@qq.com>
2022-08-03 17:13:13 +0800

    功能 2 支持并发与异步的客户端

---
+++ 2-client/main/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"log"
+	"net"
+	"rpc"
+	"time"
+)
+
+func startServer(addr chan string) {
+	// pick a free port
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	rpc.Accept(l)
+}
+
+func main() {
+	fmt.Println("day 2")
+	log.SetFlags(0)
+	addr := make(chan string)
+
+	go startServer(addr)
+	client, _ := rpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
+
+	time.Sleep(time.Second)
+	// send request & receive response
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("geerpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
+	}
+	wg.Wait()
+}
